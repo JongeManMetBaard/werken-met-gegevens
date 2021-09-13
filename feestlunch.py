60030699b9ccb1e1553d27c636609abb03b9bbff
ali <ali.gout.12@hotmail.com>
2021-09-13 09:35:15 +0200

    Berekeningen in Python

---
+++ feestlunch.py
@@ -0,0 +1,8 @@
+PrijsCroissantjes = 0.39
+AantalCroissantjes = 17
+PrijsStokbrooden = 2.78 
+AantalStokbrooden = 2
+Kortingsbon = 0.50
+AantalKortingsbonnen = 3
+
+print(PrijsCroissantjes * AantalCroissantjes + PrijsStokbrooden * AantalStokbrooden - Kortingsbon * AantalKortingsbonnen)
---
+++ speelhaldag.py
@@ -0,0 +1,7 @@
+AantalVrienden = 4
+AantalMinutenVR = 45
+MinutenPer = 5 
+EurocentPerMinuten = 0.37
+ToegangsticketPerPersoon = 7.45
+
+print(AantalVrienden * AantalMinutenVR / MinutenPer * EurocentPerMinuten + ToegangsticketPerPersoon * AantalVrienden)
